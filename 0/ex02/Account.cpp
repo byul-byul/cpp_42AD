0b8c1db2b70a6890ca34a115907db4c2ee95b8a1
B H <bh@Bs-MacBook-Pro.local>
2025-10-25 19:07:41 +0400

    ex02

--- 0/ex02/Account.cpp
+++ 0/ex02/Account.cpp
@@ -10,77 +10,139 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Account.hpp"
+# include "Account.hpp"
+# include <iostream>
+# include <ctime>
+# include <iomanip>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-Account::Account( int initial_deposit )
+Account::Account( int initial_deposit ):
+	_accountIndex(_nbAccounts),
+	_amount(initial_deposit),
+	_nbDeposits(0),
+	_nbWithdrawals(0)
 {
-	_totalAmount = initial_deposit;
-	if (_totalAmount > 0)
-		_totalNbDeposits = 1;
-	return ;
-}
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
 
-Account::Account( void )
-{
-	return ;
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex
+				<< ";amount:" << _amount
+				<< ";created" << std::endl;
 }
 
 Account::~Account( void )
 {
-	return ;
-}
-
-int	Account::getNbAccounts( void )
-{
-	return _nbAccounts;
-}
-
-int	Account::getTotalAmount( void )
-{
-	return _totalAmount;
-}
-
-int	Account::getNbDeposits( void )
-{
-	return _nbDeposits;
-}
-
-int	Account::getNbWithdrawals( void )
-{
-	
-}
-void	Account::displayAccountsInfos( void )
-{
-	
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex
+				<< ";amount:" << _amount
+				<< ";closed" << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
+	int p_amount = _amount;
+	_amount += deposit;
+	_nbDeposits++;
+
 	_totalAmount += deposit;
 	_totalNbDeposits++;
-	return ;
+
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex
+				<< ";p_amount:" << p_amount
+				<< ";deposit:" << deposit
+				<< ";amount:" << _amount
+				<< ";nb_deposits:" << _nbDeposits
+				<< std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
-	if (withdrawal > _totalAmount)
-		return false;
-	
-	_totalAmount -= withdrawal;
-	return true;
+    int p_amount = _amount;
+
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";p_amount:" << p_amount;
+
+    if (withdrawal > _amount) {
+        std::cout << ";withdrawal:refused" << std::endl;
+        return false;
+    }
+
+    _amount -= withdrawal;
+    ++_nbWithdrawals;
+    _totalAmount -= withdrawal;
+    ++_totalNbWithdrawals;
+
+    std::cout << ";withdrawal:"     << withdrawal
+              << ";amount:"         << _amount
+              << ";nb_withdrawals:" << _nbWithdrawals
+              << std::endl;
+    return true;
 }
 
 int		Account::checkAmount( void ) const
 {
-	return _totalAmount;
+	return _amount;
 }
 
 void	Account::displayStatus( void ) const
 {
-	return ;
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex
+				<< ";amount:" << _amount
+				<< ";deposits:" << _nbDeposits
+				<< ";withdrawals:" << _nbWithdrawals
+				<< std::endl;
+}
+
+int		Account::getNbAccounts( void )
+{
+	return _nbAccounts;
+}
+
+int		Account::getTotalAmount( void )
+{
+	return _totalAmount;
+}
+
+int		Account::getNbDeposits( void )
+{
+	return _totalNbDeposits;
+}
+
+int		Account::getNbWithdrawals( void )
+{
+	return _totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout	<< "accounts:" << getNbAccounts()
+				<< ";total:" << getTotalAmount()
+				<< ";deposits:" << getNbDeposits()
+				<< ";withdrawals:" << getNbWithdrawals()
+				<< std::endl;
+}
+
+void Account::_displayTimestamp(void)
+{
+	std::time_t now = std::time(0);
+	std::tm *lt = std::localtime(&now);
+
+	std::cout << "[" 
+	          << (lt->tm_year + 1900)
+	          << std::setw(2) << std::setfill('0') << lt->tm_mon + 1
+	          << std::setw(2) << std::setfill('0') << lt->tm_mday
+	          << "_"
+	          << std::setw(2) << std::setfill('0') << lt->tm_hour
+	          << std::setw(2) << std::setfill('0') << lt->tm_min
+	          << std::setw(2) << std::setfill('0') << lt->tm_sec
+	          << "] ";
 }
